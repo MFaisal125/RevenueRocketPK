847fdff37dfa17c92795e5f494bd370d80840732
MFaisal125 <mfaisal0125470@gmail.com>
2024-11-02 11:42:11 +0500

    Rocket

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    include: "**/*.jsx",
-  })],
+  plugins: [
+    react({
+      include: "**/*.jsx",
+    }),
+  ],
   build: {
     chunkSizeWarningLimit: 1600,
   },
-})
+});
