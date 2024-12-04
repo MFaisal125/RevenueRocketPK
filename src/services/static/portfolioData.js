85ff8f3834813d166247d2b64113d9f670a501b8
MFaisal125 <mfaisal0125470@gmail.com>
2024-12-04 09:27:41 +0500

    Revenue Rocket

--- src/services/static/portfolioData.js
+++ src/services/static/portfolioData.js
@@ -1,7 +1,27 @@
 import image1 from "../../assets/image/portfolio_image/p12.jpg";
 import image2 from "../../assets/image/portfolio_image/p13.jpg";
 import image3 from "../../assets/image/portfolio_image/p14.jpg";
-import image4 from "../../assets/image/portfolio_image/p16.jpg";
+import image4 from "../../assets/image/portfolio_image/p16.png";
+import image5 from "../../assets/image/portfolio_image/p1.png";
+import image6 from "../../assets/image/portfolio_image/p2.png";
+import image7 from "../../assets/image/portfolio_image/p3.png";
+import image8 from "../../assets/image/portfolio_image/p4.png";
+import image9 from "../../assets/image/portfolio_image/p5.png";
+import image10 from "../../assets/image/portfolio_image/p6.png";
+import image11 from "../../assets/image/portfolio_image/p7.png";
+import image12 from "../../assets/image/portfolio_image/p8.png";
+import image13 from "../../assets/image/portfolio_image/p9.png";
+import image14 from "../../assets/image/portfolio_image/p10.png";
+import image15 from "../../assets/image/portfolio_image/p11.png";
+import image16 from "../../assets/image/portfolio_image/p17.png";
+import image17 from "../../assets/image/portfolio_image/p18.png";
+import image18 from "../../assets/image/portfolio_image/p19.png";
+import image19 from "../../assets/image/portfolio_image/p20.png";
+import image20 from "../../assets/image/portfolio_image/p21.png";
+import image21 from "../../assets/image/portfolio_image/p22.png";
+import image22 from "../../assets/image/portfolio_image/p23.png";
+import image23 from "../../assets/image/portfolio_image/p24.png";
+import image24 from "../../assets/image/portfolio_image/p25.png";
 export const portfolioData = [
   {
     id: "1",
@@ -39,7 +59,7 @@ export const portfolioData = [
   {
     id: "2",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://next-hotel-app.vercel.app/",
     category: "Hotel Websites",
     title: "news website for bbc news tv channel this high power website",
     thumbnail: `${image4}`,
@@ -49,22 +69,20 @@ export const portfolioData = [
   {
     id: "13",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://next-hotel-app.vercel.app/",
     category: "Hotel Websites",
     title: "news website for bbc news tv channel this high power website",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image5}`,
     description:
       "When you decide it is compelling content you would like to push online, look no further than magazine and news website templates. You can use them for a niche or a general platform, meaning you can easily adjust them to your needs. It can be politics, sports, lifestyle, travel, food, health, tech, and gadgets, you name it, you can create pages of all topics.",
   },
   {
     id: "14",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://mfaisal125.github.io/resume/",
     category: "Hotel Websites",
     title: "news website for bbc news tv channel this high power website",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image6}`,
     description:
       "When you decide it is compelling content you would like to push online, look no further than magazine and news website templates. You can use them for a niche or a general platform, meaning you can easily adjust them to your needs. It can be politics, sports, lifestyle, travel, food, health, tech, and gadgets, you name it, you can create pages of all topics.",
   },
@@ -72,210 +90,192 @@ export const portfolioData = [
   {
     id: "3",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://portfolio1-123.vercel.app/",
     category: "Portfolios",
     title: "personal blogging website for my sister she is wonderfull girl",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image7}`,
     description:
       "That’s why we compiled this complete collection of personal blog themes that fit everyone. (After two years of extensive testing!)",
   },
   {
     id: "11",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://portfolio-acerenity.vercel.app/",
     category: "Portfolios",
     title: "personal blogging website for my sister she is wonderfull girl",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image8}`,
     description:
       "That’s why we compiled this complete collection of personal blog themes that fit everyone. (After two years of extensive testing!)",
   },
   {
     id: "12",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://portfolio-acerenity.vercel.app/",
     category: "Portfolios",
     title: "personal blogging website for my sister she is wonderfull girl",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image9}`,
     description:
       "That’s why we compiled this complete collection of personal blog themes that fit everyone. (After two years of extensive testing!)",
   },
   {
     id: "4",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://mfaisal125.github.io/resume3/",
     category: "Custom Websites",
     title: "Hairnic is a free single product website template crafted",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image10}`,
     description:
       "Hairnic is a free single product website template crafted to showcase your beauty and cosmetic products. With its clean and responsive design, we have poured our hearts into creating it. We ensure that Hairnic will assist you in promoting and expanding your business, serving as an ideal HTML website template for presenting your modern business. It suits various single-product businesses, including beauty products, cosmetics, skincare items, haircare products, electronics, digital goods, and much more.",
   },
   {
     id: "17",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://mfaisal125.github.io/resume2/",
     category: "Custom Websites",
     title: "Hairnic is a free single product website template crafted",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image11}`,
     description:
       "Hairnic is a free single product website template crafted to showcase your beauty and cosmetic products. With its clean and responsive design, we have poured our hearts into creating it. We ensure that Hairnic will assist you in promoting and expanding your business, serving as an ideal HTML website template for presenting your modern business. It suits various single-product businesses, including beauty products, cosmetics, skincare items, haircare products, electronics, digital goods, and much more.",
   },
   {
     id: "18",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://mfaisal125.github.io/resume2/",
     category: "Custom Websites",
     title: "Hairnic is a free single product website template crafted",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image12}`,
     description:
       "Hairnic is a free single product website template crafted to showcase your beauty and cosmetic products. With its clean and responsive design, we have poured our hearts into creating it. We ensure that Hairnic will assist you in promoting and expanding your business, serving as an ideal HTML website template for presenting your modern business. It suits various single-product businesses, including beauty products, cosmetics, skincare items, haircare products, electronics, digital goods, and much more.",
   },
   {
     id: "5",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://hotel-web2.vercel.app/",
     category: "Shopify",
     title:
       "HighTech is a meticulously crafted, creative, and modern free HTML website template for IT",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/06/it-solutions-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image13}`,
     description:
       "HighTech is a meticulously crafted, creative, and modern free HTML website template for IT solutions, perfectly suited for digital agencies. It has been specifically designed to cater to the requirements of startup digital agencies, IT solutions companies, SEO agencies, Custom Websites firms, creative agencies, web design & development agencies, online marketing agencies, SEO services providers, content marketing, consulting & financial agencies, as well as all other internet marketing and service-related agencies and businesses",
   },
   {
     id: "19",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "hhttps://hotel-web2.vercel.app/",
     category: "Shopify",
     title:
       "HighTech is a meticulously crafted, creative, and modern free HTML website template for IT",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/06/it-solutions-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image14}`,
     description:
       "HighTech is a meticulously crafted, creative, and modern free HTML website template for IT solutions, perfectly suited for digital agencies. It has been specifically designed to cater to the requirements of startup digital agencies, IT solutions companies, SEO agencies, Custom Websites firms, creative agencies, web design & development agencies, online marketing agencies, SEO services providers, content marketing, consulting & financial agencies, as well as all other internet marketing and service-related agencies and businesses",
   },
   {
     id: "20",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://hotel-web2.vercel.app/",
     category: "Shopify",
     title:
       "HighTech is a meticulously crafted, creative, and modern free HTML website template for IT",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/06/it-solutions-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image15}`,
     description:
       "HighTech is a meticulously crafted, creative, and modern free HTML website template for IT solutions, perfectly suited for digital agencies. It has been specifically designed to cater to the requirements of startup digital agencies, IT solutions companies, SEO agencies, Custom Websites firms, creative agencies, web design & development agencies, online marketing agencies, SEO services providers, content marketing, consulting & financial agencies, as well as all other internet marketing and service-related agencies and businesses",
   },
   {
     id: "6",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://e-commerce-website125.vercel.app/",
     category: "CRM Softwares",
     title:
       "Labsky is a free HTML website template designed for laboratory and science research-related businesses. ",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/04/laboratory-html-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image16}`,
     description:
       "Labsky is a free HTML website template designed for laboratory and science research-related businesses. It features a clean and modern design and a fully responsive layout that enables you to build a professional website for various institutions such as medical research labs, science research labs, innovation centers, and more",
   },
   {
     id: "21",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://e-commerce-website125.vercel.app/",
     category: "CRM Softwares",
     title:
       "Labsky is a free HTML website template designed for laboratory and science research-related businesses. ",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/04/laboratory-html-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image17}`,
     description:
       "Labsky is a free HTML website template designed for laboratory and science research-related businesses. It features a clean and modern design and a fully responsive layout that enables you to build a professional website for various institutions such as medical research labs, science research labs, innovation centers, and more",
   },
   {
     id: "22",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://next-hotel-app.vercel.app/",
     category: "CRM Softwares",
     title:
       "Labsky is a free HTML website template designed for laboratory and science research-related businesses. ",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/04/laboratory-html-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image18}`,
     description:
       "Labsky is a free HTML website template designed for laboratory and science research-related businesses. It features a clean and modern design and a fully responsive layout that enables you to build a professional website for various institutions such as medical research labs, science research labs, innovation centers, and more",
   },
   {
     id: "7",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://next-hotel-app.vercel.app/",
     category: "Website",
     title:
       "travel website user can be travel any ware any country just using our website",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image19}`,
     description:
       "Let’s face it, everyone loves to travel, but not many like to make travel pages. We decided to create travel website templates that will save you a ton of time while you travel around. Here, you can find website templates for travel agencies, blogs, hotels, directories, villas, informative sites, and so much",
   },
   {
     id: "8",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://next-e-commerce1.vercel.app/",
     category: "Website",
     title:
       "travel website user can be travel any ware any country just using our website",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image20}`,
     description:
       "Let’s face it, everyone loves to travel, but not many like to make travel pages. We decided to create travel website templates that will save you a ton of time while you travel around. Here, you can find website templates for travel agencies, blogs, hotels, directories, villas, informative sites, and so much",
   },
   {
     id: "10",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://mfaisal125.github.io/resume2/",
     category: "Website",
     title:
       "travel website user can be travel any ware any country just using our website",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image21}`,
     description:
       "Let’s face it, everyone loves to travel, but not many like to make travel pages. We decided to create travel website templates that will save you a ton of time while you travel around. Here, you can find website templates for travel agencies, blogs, hotels, directories, villas, informative sites, and so much",
   },
   {
     id: "9",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://mfaisal125.github.io/resume2/",
     category: "POS Website",
     title:
       "travel website user can be travel any ware any country just using our website",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image22}`,
     description:
       "Let’s face it, everyone loves to travel, but not many like to make travel pages. We decided to create travel website templates that will save you a ton of time while you travel around. Here, you can find website templates for travel agencies, blogs, hotels, directories, villas, informative sites, and so much",
   },
   {
     id: "23",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://mfaisal125.github.io/resume/",
     category: "POS Website",
     title:
       "travel website user can be travel any ware any country just using our website",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image23}`,
     description:
       "Let’s face it, everyone loves to travel, but not many like to make travel pages. We decided to create travel website templates that will save you a ton of time while you travel around. Here, you can find website templates for travel agencies, blogs, hotels, directories, villas, informative sites, and so much",
   },
   {
     id: "25",
 
-    live_url: "https://monabbirhasan.github.io/FantasyFocus.com/",
+    live_url: "https://mfaisal125.github.io/resume/",
     category: "POS Website",
     title:
       "travel website user can be travel any ware any country just using our website",
-    thumbnail:
-      "https://i0.wp.com/htmlcodex.com/wp-content/uploads/2023/05/single-product-website-template.jpg?resize=740%2C463&ssl=1",
+    thumbnail: `${image24}`,
     description:
       "Let’s face it, everyone loves to travel, but not many like to make travel pages. We decided to create travel website templates that will save you a ton of time while you travel around. Here, you can find website templates for travel agencies, blogs, hotels, directories, villas, informative sites, and so much",
   },
